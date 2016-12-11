12869b7b138f743b9ca7b8b52f751bb67efe79fc
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-11 08:55:40 +0700

    simple handler for searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' }
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render () {
     return (
       <form className="input-group">
-        <input className="form-control" />
+        <input
+          placeholder="get a five-day forecast in your favorite cities"
+          value={this.state.term}
+          onChange={this.onInputChange}
+          className="form-control" />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Search</button>
         </span>
