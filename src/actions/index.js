ffd52bf77cf997031438d0fd4d6def0cdfd28efb
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-12 09:13:27 +0700

    ajax using axios

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = "47ea6d165ae12c73e479c6c5dbb10868";
+export const FETCH_WEATHER = 'FETCH_WEATHER'; 
+const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}";
+
+export function fetchWeather(city) {
+  const url = "${ROOT_URL}&q=${city},us";
+
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
