4a685b1490a949270435f1b3fe3d8763d5ce3e38
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2016-12-13 09:37:26 +0700

    missing apply middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,6 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-
   const request = axios.get(url);
   console.log('request: ', request);
 
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,13 +1,10 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action){
   console.log('Action received', action);
   switch (action.type) {
     case FETCH_WEATHER:
-      // return [ action.payload.data ]; , just signle city
-      // return state.push(action.payload.data); multiple cities, mutating state
-      // return [ action.payload.data, ...state ] this one like concat
-      return state.concat(action.payload.data); // return new instance state
+      return [ action.payload.data, ...state ];
   }
   return state;
 }
